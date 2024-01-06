309cbada1a1f18153520fae18af47cc02addf648
SaleemAlmahde <almahdesaleem@gmail.com>
2024-01-06 18:12:42 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import './assets/css/style.css'
+import CategoryPage from "./pages/website/CategoryPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <Routes>
+            <Route index element={<CategoryPage />} />
+            {/* <Route path="/products" element={<ProductsPage />} />
+            <Route path="/add" element={<AddProduct />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/page" element={<Page />} />
+            <Route path="/admin" element={<DashboardHomePage />} /> */}
+          </Routes>
+        </Router>
   );
 }
 
